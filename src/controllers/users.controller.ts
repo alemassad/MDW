20d3423f0edb96c537b687ce51882503e6582ac3
ale massad <massadale@hotmail.com>
2025-05-11 10:27:50 -0300

    creacion del User controlador

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,13 +1,41 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { User } from "../models";
 
 export const controller = (req: Request, res: Response) => {
   const { name } = req.params;
   res.json({ user: name });
 };
-export const createUser = (req: Request, res: Response) => {
-  const { name } = req.params;
-  res.json({ user: name });
+
+interface UserBody {
+  name: string;
+  lastname: string;
+  birthdate: string;
+  email: string;
+  isAdmin?: boolean;
+}
+
+
+export const createUser = async (
+  req: Request<{}, {}, UserBody>,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const formatedDate = new Date(req.body.birthdate);
+  try {
+    const newUser = await User.create({
+      ...req.body,
+      birthdate: formatedDate,
+    });
+      res.status(201).json({
+      message: "User created successfully",
+      data: newUser,
+      error: false,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
+
 export const getUsers = (req: Request, res: Response) => {
   const { name } = req.params;
   res.json({ user: "alguien" });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { json, Request, Response } from "express";
 import router from "./routes/index";
 import dotenv from "dotenv";
 import connectDB from "./database";
@@ -11,6 +11,8 @@ const port = process.env.PORT || 3000;
 
 connectDB();
 syncDatabase();
+
+app.use(json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hola campeon");
